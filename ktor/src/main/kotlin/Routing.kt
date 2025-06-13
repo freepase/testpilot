3146ef3ddfa8a83589cae2fffc36684c71589282
Paul <paul.seddon@gmail.com>
2025-06-13 21:37:27 +0100

    TV endpoint

--- ktor/src/main/kotlin/Routing.kt
+++ ktor/src/main/kotlin/Routing.kt
@@ -1,18 +1,11 @@
 package ai.lwcap
 
-import com.fasterxml.jackson.databind.*
 import io.ktor.http.*
-import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
-import io.ktor.server.html.*
-import io.ktor.server.plugins.calllogging.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.webjars.*
-import kotlinx.html.*
-import org.slf4j.event.*
 
 fun Application.configureRouting() {
     install(Webjars) {
@@ -20,7 +13,12 @@ fun Application.configureRouting() {
     }
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("lone wolf capital")
+        }
+        post("/tv") {
+            val text = call.receiveText()
+            log.info("Received: {}", text)
+            call.respondText("ok")
         }
         get("/webjars") {
             call.respondText("<script src='/webjars/jquery/jquery.js'></script>", ContentType.Text.Html)
